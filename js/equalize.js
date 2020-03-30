1d592c3ee8ec2191dd2115dcd16e224dfddcff61
Daniel Ospina <daniel.ospina@payulatam.com>
2020-03-30 16:31:35 -0500

    modified script location and removed console.log

--- js/equalize.js
+++ js/equalize.js
@@ -1,10 +1,8 @@
 const params = new URLSearchParams(window.location.search);
-console.log(window.location.search);
 
 var table = document.getElementById("mainTable");
 
 for (const [key, value] of params) {
-    console.log(key + "  " + value);
     var row = table.insertRow(0);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);

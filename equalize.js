09c65b9727b24f28f685e4dacd35eba8cd59f5c9
Daniel Ospina <daniel.ospina@payulatam.com>
2020-03-30 18:16:44 -0500

    append row instead of insert first

--- equalize.js
+++ equalize.js
@@ -3,7 +3,7 @@ const params = new URLSearchParams(window.location.search);
 var table = document.getElementById("mainTable");
 
 for (const [key, value] of params) {
-    var row = table.insertRow(0);
+    var row = table.insertRow();
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     cell1.innerHTML = key;
